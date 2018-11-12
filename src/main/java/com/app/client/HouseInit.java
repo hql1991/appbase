a68e6595c2a556f6d109a627a4add023bc277d76
Qianli-Mac <qianli.hu@sv.cmu.edu>
2018-11-12 11:20:54 -0800

    Fix House bug

--- src/main/java/com/app/client/HouseInit.java
+++ src/main/java/com/app/client/HouseInit.java
@@ -12,13 +12,13 @@ public class HouseInit {
     private static HouseService houseService = HouseService.getInstance();
 
     public static String[] init(String[] argv) {
-        doDeleteAll("5bdf54785861e26d0cf9e3cc");
+        doDeleteAll();
         String[] ids = new String[4];
         ids[0] = doPost(argv[0], "70 Julia Cr", "CA", "94043", "1990");
         ids[1] = doPost(argv[1], "1122 Lakeside Drive", "CA", "94555", "2010");
         ids[2] = doPost(argv[2], "8 Mary Street", "CA", "95035", "2000");
         ids[3] = doPost(argv[3], "123 Sky Way", "CA", "94070", "2009");
-        doGetAll("5bdf54785861e26d0cf9e3cc");
+        doGetAll();
         return ids;
     }
 
@@ -37,7 +37,7 @@ public class HouseInit {
     }
 
 
-    public static void doGetAll(String ownerid) {
+    public static void doGetAll() {
         try {
             URL url = new URL("http://localhost:8080/api/houses");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -61,7 +61,7 @@ public class HouseInit {
         }
     }
 
-    public static void doDeleteAll(String ownerid) {
+    public static void doDeleteAll() {
         try {
             URL url = new URL("http://localhost:8080/api/houses");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
